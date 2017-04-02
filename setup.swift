4aa6504604df6a231eb1d91ebaf7e8a9dc454398
Lukas Valine <lukas@valine.io>
2017-04-02 12:49:30 -0500

    added setup function

---
+++ setup.swift
@@ -0,0 +1,18 @@
+//
+//  setup.swift
+//  
+//
+//  Created by Lukas Valine on 4/2/17.
+//
+//
+
+import Foundation
+import PlaygroundSupport
+import UIKit
+
+public func _setup() {
+    let viewController = GameViewController()
+    
+    PlaygroundPage.current.liveView = viewController
+}
+
